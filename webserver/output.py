3d841d9c3f7d3832ad442fa6067cc33486263b38
Adam Dodge <adam.dodge@colorado.edu>
2019-06-24 14:49:59 -0600

    debugging

--- webserver/output.py
+++ webserver/output.py
@@ -49,7 +49,7 @@ class RinexWrite:
         self.lon = lon
         self.alt = alt
         self.content = ''
-        self.leapS = leapS
+        self.leapS = int(leapS)
         if os.path.isfile(self.fname):
             fix_rinex(self.fname)
         else:

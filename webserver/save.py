4b5673ef436259927f0979f46bd3f2e92a38135c
Adam Dodge <adam.dodge@colorado.edu>
2019-06-27 19:38:07 -0600

    Debbuging slow gps save

--- webserver/save.py
+++ webserver/save.py
@@ -12,19 +12,16 @@ def save_lidar(data, data_directory, loc):
         print("No data in LiDAR packet. ")
         return
     unix_time = struct.unpack('<q', data[0:8])[0]  # First thing is unix time
-    print(unix_time)
-     # Find day and hour
-    dayhour = dt.datetime(1970, 1, 1) + dt.timedelta(seconds=unix_time/2) + dt.timedelta(seconds=unix_time/2)
-    print(dayhour)
-    tvec, measvec = [], []  # Initialization
+    hour = dt.datetime(1970, 1, 1) + dt.timedelta(seconds=unix_time)  # Find day and hour
+    t_vec, meas_vec = [], []  # Initialization
     num = (len(data)-8)/6  # Number of measurements
     for i in range(int(num)):
         t, meas = struct.unpack('<LH', data[8+i*6:8+(i+1)*6])  # Unpack data
-        tvec.append(dayhour + dt.timedelta(microseconds=t))
-        measvec.append(meas)
+        t_vec.append(hour + dt.timedelta(microseconds=t))
+        meas_vec.append(meas)
     try:
-        with open(os.path.join(data_directory, loc, 'lidar', dayhour.strftime('%Y-%m-%d.txt')), 'a+') as f:
-            for i, j in zip(tvec, measvec):
+        with open(os.path.join(data_directory, loc, 'lidar', hour.strftime('%Y-%m-%d.txt')), 'a+') as f:
+            for i, j in zip(t_vec, meas_vec):
                 f.write(f'{i} {j}\n')
     except FileNotFoundError:
         print("Data directory is bad. Try again. ")
@@ -38,28 +35,27 @@ def save_raw_gps(data, data_directory, loc, lat, lon, alt):
     end = len(data)
     # Do the rinex thing
     while counter < end:
-        rcvTOW, week, leapS, numMeas = struct.unpack('<dHbB', data[counter:counter+12])
+        print('Save 41:', dt.datetime.utcnow())
+        rcv_tow, week, leap_s, num_meas = struct.unpack('<dHbB', data[counter:counter+12])
         counter += 12
-        wrtr = RinexWrite(os.path.join(data_directory, loc, 'rawgps'), lat, lon, alt, week, rcvTOW, leapS, loc)
-        pseudorange = []
-        carrier_phase = []
-        doppler = []
-        gnssId = []
-        svId = []
-        sigId = []
-        cno = []
-        for i in range(numMeas):
+        writer = RinexWrite(os.path.join(data_directory, loc, 'rawgps'), lat, lon, alt, week, rcv_tow, leap_s, loc)
+        pseudorange, carrier_phase, doppler, gnss_id, sv_id, sig_id, cno = [], [], [], [], [], [], []
+        for i in range(num_meas):
             pr, cp, do, other = struct.unpack('ddfH', data[counter:counter+22])
             counter += 22
             pseudorange.append(pr)
             carrier_phase.append(cp)
             doppler.append(do)
-            gnssId.append((other >> 12) & 0x07)
-            svId.append((other >> 6) & 0x3f)
-            sigId.append((other >> 3) & 0x07)
+            gnss_id.append((other >> 12) & 0x07)
+            sv_id.append((other >> 6) & 0x3f)
+            sig_id.append((other >> 3) & 0x07)
             cno.append(other & 0x07)
-        p = RxmRawx(rcvTOW, week, leapS, numMeas, pseudorange, carrier_phase, doppler, gnssId, svId, sigId, cno)
-        wrtr.write_data(p)
+        print('Save 62:', dt.datetime.utcnow())
+        p = RxmRawx(rcv_tow, week, leap_s, num_meas, pseudorange, carrier_phase, doppler, gnss_id, sv_id, sig_id, cno)
+        print('Save 64:', dt.datetime.utcnow())
+        writer.write_data(p)
+        print('Save 66:', dt.datetime.utcnow())
+    print('Save 67:', dt.datetime.utcnow())
 
 
 def save_gps_pos(data, data_directory, loc):
@@ -67,13 +63,13 @@ def save_gps_pos(data, data_directory, loc):
     if len(data) != 30:
         print("Data is incorrect. ")
         return
-    itow, week, lon, lat, height = struct.unpack('<IHddd', data)
-    t = dt.datetime(1980, 1, 6) + dt.timedelta(days=7*week, microseconds=itow*1000)
-    fname = os.path.join(data_directory, loc, 'position', t.strftime('%Y-%m-%d.txt'))
-    if os.path.isfile(fname):  # If file exists make sure it doesnt need to be fixed
-        fix_hppos(fname)
+    i_tow, week, lon, lat, height = struct.unpack('<IHddd', data)
+    t = dt.datetime(1980, 1, 6) + dt.timedelta(days=7*week, microseconds=i_tow*1000)
+    f_name = os.path.join(data_directory, loc, 'position', t.strftime('%Y-%m-%d.txt'))
+    if os.path.isfile(f_name):  # If file exists make sure it doesnt need to be fixed
+        fix_hppos(f_name)
     try:
-        with open(fname, 'a+') as f:
+        with open(f_name, 'a+') as f:
             f.write(f'{t} {lat} {lon} {height}\n')  # Write
     except FileNotFoundError:
         print('Data directory is bad. Try again. ')

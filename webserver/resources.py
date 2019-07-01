27f5eefc074657b5e14de6979291caa2ac969d16
Adam Dodge <adam.dodge@colorado.edu>
2019-07-01 15:03:40 -0600

    Changing from file saving to database.

--- webserver/resources.py
+++ webserver/resources.py
@@ -19,11 +19,12 @@ def read_key(fname):
 
 def decode_msg(m, key):
     """ Function to decode message with the key. """
-    try:
-        time = jwt.decode(m, key, algorithm='RS256')['t']
-        td = ((dt.datetime.utcnow() - dt.datetime(1970, 1, 1)).total_seconds() - float(time))
-    except:
-        return False
+    print(key)
+    #try:
+    time = jwt.decode(m, key, algorithm='RS256')['t']
+    td = ((dt.datetime.utcnow() - dt.datetime(1970, 1, 1)).total_seconds() - float(time))
+    #except:
+    #    return False
     if td < 10:
         return True
     return False
